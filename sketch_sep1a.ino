3dd0b8af3a3f4631f526143b6298864d1df3c855
Aryan9001 <122262530+Aryan9001@users.noreply.github.com>
2023-09-01 16:15:02 +0530

    Add files via upload

---
+++ sketch_sep1a.ino
@@ -0,0 +1,66 @@
+#include "arduino_secrets.h" 
+#include <WiFiNINA.h>
+int intense;
+
+char ssid[] = OnePlus;
+
+char pass[] = 1234567890;
+
+
+WiFiClient client;
+char   HOST_NAME[] = "maker.ifttt.com";
+
+String PATH_NAME   = "/trigger/Sunlight_capture/with/key/dnaRDV8XvdbVFbudVUbFOXFl8Tbywg12Q8Ya38uOMng"; 
+
+String queryString = "?value1=57&value2=25";
+
+
+void setup() {
+  WiFi.begin(ssid, pass);
+
+
+  Serial.begin(9600);
+
+  while (!Serial);
+
+  if (client.connect(HOST_NAME, 80)) {
+
+    Serial.println("Connected to server");
+
+  }
+
+  else {
+    Serial.println("connection failed");
+  }
+
+}
+
+
+void loop() {
+
+  intense = analogRead(A5);
+  Serial.println(intense);
+  if (intense > 620) {
+
+    client.println("GET " + PATH_NAME + queryString + " HTTP/1.1");
+
+    client.println("Host: " + String(HOST_NAME));
+
+    client.println("Connection: close");
+
+    client.println(); 
+    while (client.connected()) {
+
+      if (client.available()) {
+        char c = client.read();
+        Serial.print(c);
+      }
+
+    }
+    client.stop();
+    Serial.println();
+    Serial.println("disconnected");
+
+}
+
+}
